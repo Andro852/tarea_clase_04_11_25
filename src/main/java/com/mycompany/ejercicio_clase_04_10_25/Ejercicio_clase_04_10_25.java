3adda11435234ffca984107281f68feade3a7162
Jonatan <Jonatan@DESKTOP-PICSI76>
2025-10-04 17:50:29 -0600

    tarea clase 04/10/2025

---
+++ src/main/java/com/mycompany/ejercicio_clase_04_10_25/Ejercicio_clase_04_10_25.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_clase_04_10_25;
+
+/**
+ *
+ * @author Jonatan
+ */
+public class Ejercicio_clase_04_10_25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
